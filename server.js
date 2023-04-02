e1a79bdfbcf6173e5bb6b84082e2a6d08ecfd701
Mathis Telschow <mathistelschow@gmail.com>
2023-04-02 22:29:55 +0200

    Link the login to the backend

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 import express from 'express';
-import bcrypt from 'bcrypt-nodejs'
+import bcrypt from 'bcrypt-nodejs';
+import cors from 'cors';
 
 const app = express();
-
-app.use(express.json());
 
 const database = {
 	users: [
@@ -26,11 +25,15 @@ const database = {
 	],
 };
 
+app.use(express.json());
+app.use(cors());
+
 app.get('/', (req, res) => {
 	res.send(database.users);
 });
 
 app.post('/signin', (req, res) => {
+	console.log(req.body.email, req.body.password);
 	if (
 		req.body.email === database.users[0].email &&
 		req.body.password === database.users[0].password
@@ -55,33 +58,33 @@ app.post('/register', (req, res) => {
 });
 
 app.get('/profil/:id', (req, res) => {
-    const { id } = req.params;
-    let found = false;
-    database.users.forEach((user) => {
+	const { id } = req.params;
+	let found = false;
+	database.users.forEach((user) => {
 		if (user.id === id) {
-            found = true;
+			found = true;
 			return res.json(user);
 		}
 	});
-    if (!found){
-        res.status(404).json('no such user');
-    }
+	if (!found) {
+		res.status(404).json('no such user');
+	}
 });
 
 app.put('/image', (req, res) => {
-    const { id } = req.body;
-    let found = false;
-    database.users.forEach((user) => {
+	const { id } = req.body;
+	let found = false;
+	database.users.forEach((user) => {
 		if (user.id === id) {
-            found = true;
-            user.entries++;
+			found = true;
+			user.entries++;
 			return res.json(user.entries);
 		}
 	});
-    if (!found){
-        res.status(404).json('no such user');
-    }
-})
+	if (!found) {
+		res.status(404).json('no such user');
+	}
+});
 
 app.listen(3000, () => {
 	console.log('App is running on port 3000');
