44ab7737c46b0026e262c7f492342148e4fcbab4
Mathis Telschow <mathistelschow@gmail.com>
2023-04-05 19:12:21 +0200

    Require signin to have inputs

--- constrollers/signin.js
+++ constrollers/signin.js
@@ -1,5 +1,8 @@
 const handleSignin = (req, res, db, bcrypt) => {
 	const { email, password } = req.body;
+	if (!email || !password) {
+		return res.status(400).json('incorrect form submission');
+	}
 
 	db.select('email', 'hash')
 		.from('login')
