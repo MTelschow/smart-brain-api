b893bdaf5bdb76f16c04195039b236a75520e46e
Mathis Telschow <mathistelschow@gmail.com>
2023-04-05 15:53:10 +0200

    Seperate register from server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import bcrypt from 'bcrypt-nodejs';
 import cors from 'cors';
 import knex from 'knex';
 
+import handleRegister from './constrollers/register.js';
+
 const db = knex({
 	client: 'pg',
 	connection: {
@@ -49,35 +51,7 @@ app.post('/signin', (req, res) => {
 		});
 });
 
-app.post('/register', (req, res) => {
-	const { email, name, password } = req.body;
-
-	const hash = bcrypt.hashSync(password);
-
-	db.transaction((trx) => {
-		trx
-			.insert({
-				hash: hash,
-				email: email,
-			})
-			.into('login')
-			.returning('email')
-			.then((loginEmail) => {
-				return trx('users')
-					.returning('*')
-					.insert({
-						email: loginEmail[0].email,
-						name: name,
-						joined: new Date(),
-					})
-					.then((user) => {
-						res.json(user[0]);
-					});
-			})
-			.then(trx.commit)
-			.catch(trx.rollback);
-	}).catch((err) => res.status(400).json('unable to register'));
-});
+app.post('/register', (req, res) => handleRegister(req, res, db, bcrypt));
 
 app.get('/profil/:id', (req, res) => {
 	const { id } = req.params;
